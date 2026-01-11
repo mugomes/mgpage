3f6fa6a83e70a31031e400d78b50cd6f62921c77
mugomes <murilogomes5792@gmail.com>
2026-01-11 20:10:13 -0300

    Added file

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Mu Gomes</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+</head>
+<body>
+<article>
+    <h1>Em breve!</h1>
+    <div>
+        <p>Em desenvolvimento!</p>
+    </div>
+</article>
+</body>
+</html>
