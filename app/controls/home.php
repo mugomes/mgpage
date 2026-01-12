13064f4534fdf87029df3e17d0da18b3370552bc
mugomes <murilogomes5792@gmail.com>
2026-01-12 13:26:45 -0300

    Test speed

--- app/controls/home.php
+++ app/controls/home.php
@@ -5,3 +5,15 @@ if (!defined('MGPAGE')) {
 
 $tpl->var('siteTitle', 'MGPage');
 $tpl->includeFile('content', $appRoot . '/views/home.html');
+
+$start = microtime(true);
+
+$sum = 0;
+for ($i = 1; $i <= 100000; $i++) {
+    $sum += $i;
+}
+
+// Calcula tempo decorrido
+$elapsed = microtime(true) - $start;
+
+$tpl->var('tempo', $elapsed);
